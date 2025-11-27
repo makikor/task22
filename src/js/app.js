2eec9510548632cf36b61faff7e3918c8013428c
Maksim <Mikimiki89@yandex.ru>
2025-11-27 03:56:10 +0300

    1

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+// TODO: write your code here
+import sum from './basic';
+import {obj, orderByProps} from '../js/forIn';
+
+orderByProps(obj, ["name", "level"])
+
+console.log('worked');
+
+console.log(sum([1, 2]));
---
+++ src/js/forIn.js
@@ -0,0 +1,22 @@
+export const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40}
+
+export function orderByProps(obj, arr){
+    const newArr = [];
+    const x =[]
+
+    for (const i in obj) {
+        if (!arr.includes(i)) {
+        newArr.push({key: i, value: obj[i]});
+        } else {
+            x.push({key: i, value: obj[i]});
+        }
+    }
+    newArr.sort((a, b) => b.value - a.value);
+
+    for(let i =x.length-1; i>=0; i--){
+        newArr.unshift(x[i]);
+    }
+
+
+    return (newArr);
+}
